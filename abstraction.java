0f55ab331772312b7962e6d74ff31f3a6622410d
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-04-22 20:18:58 +0100

    working

--- abstraction.java
+++ abstraction.java
@@ -6,18 +6,43 @@ abstract class ball{
     public ball(String m, int t, float v){
 
         material = m;
-        volume  = v;
         texture = t;
+        volume  = v;
     }
     public abstract void displayball();
+
+    public int getTexture(){
+        return texture;
+    }
+    public float getVolume(){
+        return volume;
+    }
+    public String getMaterial(){
+        return material;
+    }
+
+
 }
 class basketball extends ball{
 
     private String pattern;
-    public basketball (String p){
+    public basketball (String m, int t, float v, String p){
 
-        super(m,v,t);
+        super(m,t,v);
         pattern  = p;
 
+    }
+    public void displayball(){
+
+        System.out.println("the material is: "+getMaterial()+ "the texture is code: "+getTexture()+ "the volume is: "+getVolume()+ "the pattern is: "+pattern);
+        
+    }
+}
+
+public class abstraction{
+
+    public static void main(String[] args) {
+        basketball x = new basketball("plastic", 5, 32, "wilson");
+        x.displayball();
     }
 }
